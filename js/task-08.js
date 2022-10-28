0a8c2a96555c07807e2a09648eb0121f7ab696a6
Pohorielova <anna.pogorielova@gmail.com>
2022-10-28 20:56:32 +0200

    Update task-09.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,40 @@
 
+// 1.Обработка отправки формы form.login-form должна быть по событию submit.
+// 2.При отправке формы страница не должна перезагружаться.
+// 3.Если в форме есть незаполненные поля, выводи alert с предупреждением о том,
+//  что все поля должны быть заполнены.
+// 4.Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект,
+//  где имя поля будет именем свойства, а значение поля - значением свойства. 
+//  Для доступа к элементам формы используй свойство elements.
+// 5.Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+{/* <form class="login-form">
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <button type="submit">Login</button>
+</form> */}
+
+// 1.пойти найти взять форму 
+const form =document.querySelector('.login-form');
+// 2. повесить событие на форму
+form.addEventListener('submit', function (event){
+// убрать перегазругку
+    event.preventDefault();
+// проверить заплненіе поля, если не - алерт
+const {email, password} = this.elements;
+if(!email.value || !password.value) {
+alert("Все поля должны быть заполнены")
+}
+// собрать данные
+
+      console.log(email.value, password.value);
+// ресетнуть форму
+      form.reset();
+});
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Напиши скрипт, который изменяет цвета фона элемента <body> 
+// через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+// 1. пойти найти взять боди и кнопку и спан
+const body =document.querySelector('body');
+const btn =document.querySelector('.change-color');
+const colorText =document.querySelector('.color');
+// 2. повесить собітие на кнопку
+btn.addEventListener('click', event=>{
+
+})
